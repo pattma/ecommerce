fa2735702efcdbd5c51a696a48569ab1411e740d
pattma <pattarin.sine.m@gmail.com>
2023-10-21 15:43:18 +0700

    fix bugs

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -7,37 +7,42 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const fs = require("fs");
 
 router.post("/create-account", upload.single("file"), async (req, res, next) => {
-    const { name, email, password } = req.body;
-    const userEmail = await User.findOne({ email });
+    try {
+      const { name, email, password } = req.body;
+      const userEmail = await User.findOne({ email });
+  
+      if (userEmail) {
+        const filename = req.file.filename;
+        const filePath = `uploads/${filename}`;
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ message: "Error deleting file" });
+          } else {
+            res.json({ message: "File deleted successfully" });
+          }
+        });
+        return next(new ErrorHandler("User already exists", 400));
 
-    if (userEmail) {
+      }
+  
+      // Profile photo from upload on Sign up Page
       const filename = req.file.filename;
-      const filePath = `uploads/${filename}`;
-      fs.unlink(filePath, (err) => {
-        if (err) {
-          console.log(err);
-          res.status(500).json({ message: "Error deleting file" });
-        } 
-        // else {
-        //   res.json({ message: "File deleted successfully" });
-        // }
-      });
-      return next(new ErrorHandler("User already exists", 400));
+      const fileUrl = path.join(filename);
+  
+      const user = {
+        name: name,
+        email: email,
+        password: password,
+        avatar: fileUrl, // Profile photo
+      };
+
+      const newUser = await User.create(user);
+      res.status(201).json({ success: true, newUser });
+
+    } catch (e) {
+      return next(new ErrorHandler(e.message), 400);
     }
-
-    // Profile photo from upload on Sign up Page
-    const filename = req.file.filename;
-    const fileUrl = path.join(filename);
-
-    const user = {
-      name: name,
-      email: email,
-      password: password,
-      avatar: fileUrl, // Profile photo
-    };
-
-    const newUser = await User.create(user);
-    res.status(201).json({ success: true, newUser });
   }
 );
 
--- backend/model/user.js
+++ backend/model/user.js
@@ -17,31 +17,31 @@ const userSchema = new mongoose.Schema({
     minLength: [4, "Password should be greater than 4 characters"],
     select: false,
   },
-  phoneNumber: {
-    type: Number,
-  },
-  addresses: [
-    {
-      country: {
-        type: String,
-      },
-      city: {
-        type: String,
-      },
-      address1: {
-        type: String,
-      },
-      address2: {
-        type: String,
-      },
-      zipCode: {
-        type: Number,
-      },
-      addressType: {
-        type: String,
-      },
-    },
-  ],
+  // phoneNumber: {
+  //   type: Number,
+  // },
+  // addresses: [
+  //   {
+  //     country: {
+  //       type: String,
+  //     },
+  //     city: {
+  //       type: String,
+  //     },
+  //     address1: {
+  //       type: String,
+  //     },
+  //     address2: {
+  //       type: String,
+  //     },
+  //     zipCode: {
+  //       type: Number,
+  //     },
+  //     addressType: {
+  //       type: String,
+  //     },
+  //   },
+  // ],
   role: {
     type: String,
     default: "user",
